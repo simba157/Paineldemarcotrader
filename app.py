37579b4e3858869d22739f71f03690145de8a22c
simba157 <marcosconsegue@gmail.com>
2025-05-11 19:25:29 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Painel Asiático", layout="wide")
+
+st.title("Painel Asiático ZETA")
+st.markdown("Este painel vai exibir sinais sniper em tempo real.")
+
+st.success("Painel carregado com sucesso!")
